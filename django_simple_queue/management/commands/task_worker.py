8b863e86fa461cf2654447822d4a5f95ca06bc2a
Dr. Shubham Dipt <shubham.dipt@gmail.com>
2024-12-16 16:20:51 +0100

    [feature] event loop fix

--- django_simple_queue/management/commands/task_worker.py
+++ django_simple_queue/management/commands/task_worker.py
@@ -11,41 +11,56 @@ import random
 import traceback
 
 
+
+class ManagedEventLoop:
+    def __init__(self):
+        self.loop = None
+
+    def __enter__(self):
+        try:
+            self.loop = asyncio.get_running_loop()
+        except RuntimeError:
+            self.loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(self.loop)
+        return self.loop
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        if self.loop is not None:
+            self.loop.close()
+
+
+
 def process_task(task_id):
     task_obj = Task.objects.get(id=task_id)
     print(f"Initiating task id: {task_id}")
     if task_obj.status == Task.QUEUED:  # One more extra check to make sure
-        try:
-            path = task_obj.task.split('.')
-            module = importlib.import_module('.'.join(path[:-1]))
-            func = getattr(module, path[-1])
-            args = json.loads(task_obj.args)
-            task_obj.output = ""
-            task_obj.status = Task.PROGRESS
-            task_obj.save()
-
-            # In case event loop gets killed
+        # In case event loop gets killed
+        with ManagedEventLoop() as loop:
             try:
-                asyncio.set_event_loop(asyncio.get_running_loop())
-            except RuntimeError:
-                asyncio.set_event_loop(asyncio.new_event_loop())
-
-            if inspect.isgeneratorfunction(func):
-                for i in func(**args):
-                    output = i
-                    task_obj.output += output
-                    task_obj.save()
-            else:
-                task_obj.output = func(**args)
+                path = task_obj.task.split('.')
+                module = importlib.import_module('.'.join(path[:-1]))
+                func = getattr(module, path[-1])
+                args = json.loads(task_obj.args)
+                task_obj.output = ""
+                task_obj.status = Task.PROGRESS
                 task_obj.save()
-            task_obj.status = Task.COMPLETED
-            task_obj.save()
-        except Exception as e:
-            task_obj.output += f"{repr(e)}\n\n{traceback.format_exc()}"
-            task_obj.status = Task.FAILED
-            task_obj.save()
-        finally:
-            print(f"Finished task id: {task_id}")
+
+                if inspect.isgeneratorfunction(func):
+                    for i in func(**args):
+                        output = i
+                        task_obj.output += output
+                        task_obj.save()
+                else:
+                    task_obj.output = func(**args)
+                    task_obj.save()
+                task_obj.status = Task.COMPLETED
+                task_obj.save()
+            except Exception as e:
+                task_obj.output += f"{repr(e)}\n\n{traceback.format_exc()}"
+                task_obj.status = Task.FAILED
+                task_obj.save()
+            finally:
+                print(f"Finished task id: {task_id}")
 
 
 class Command(BaseCommand):
